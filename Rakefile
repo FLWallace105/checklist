e6d3de23f85e02de0e41a86386d5b9bce5c49fcd
Floyd Wallace <floydwallace@floydwallace-mac.attlocal.net>
2022-08-24 17:03:42 -0700

    initial working commit (sort of)

---
+++ Rakefile
@@ -0,0 +1,19 @@
+require 'dotenv'
+require 'active_record'
+require 'shopify_api'
+require 'sinatra/activerecord/rake'
+
+require_relative 'checklist'
+
+#require 'active_record/railties/databases.rake'
+
+Dotenv.load
+
+
+namespace :pull_shopify do
+    desc 'Pull down the prospective rollover month collection and products, metafields etc'
+    task :get_next_month_collection do |t|
+        Checklist::ShopifyGetter.new.shopify_get_all_resources
+    end
+
+end
---
+++ checklist.rb
@@ -0,0 +1,61 @@
+#checklist.rb
+require 'dotenv'
+require 'httparty'
+require 'shopify_api'
+require 'active_record'
+require 'sinatra/activerecord'
+#require 'logger'
+
+Dotenv.load
+Dir[File.join(__dir__, 'lib', '*.rb')].each { |file| require file }
+Dir[File.join(__dir__, 'models', '*.rb')].each { |file| require file }
+
+module Checklist
+  class ShopifyGetter
+    
+    
+
+    def initialize
+      @shopname = ENV['SHOPIFY_SHOP_NAME']
+      @api_key = ENV['SHOPIFY_API_KEY']
+      @password = ENV['SHOPIFY_API_PASSWORD']
+      @secret = ENV['SHOPIFY_SHARED_SECRET']
+      @app_token = ENV['APP_TOKEN']
+
+      
+    end
+
+    def shopify_get_all_resources
+   
+      puts "Starting all shopify resources download"
+    #   shop_url = "https://#{@api_key}:#{@password}@#{@shopname}.myshopify.com/admin"
+    #   puts shop_url
+      
+    #   ShopifyAPI::Base.site = shop_url
+    #   ShopifyAPI::Base.api_version = '2020-04'
+    #   ShopifyAPI::Base.timeout = 180
+
+    puts "#{@api_key}, #{@secret}, #{@shopname}"
+
+
+      ShopifyAPI::Context.setup(
+        api_key: "DUMMY",
+        api_secret_key: @app_token,
+        scope: "DUMMY",
+        host_name: "DUMMY",
+        private_shop: "elliestaging.myshopify.com",
+        session_storage: ShopifyAPI::Auth::FileSessionStorage.new,
+        is_embedded: false, 
+        is_private: true, 
+        api_version: "2022-07"
+        
+      )
+
+      product_count = ShopifyAPI::Product.count()
+
+      puts "We have #{product_count.inspect} products in Ellie now"
+
+    end
+
+end
+end
